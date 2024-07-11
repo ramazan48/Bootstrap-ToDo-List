a15baa399ad3c39d0d72d61993cd22b5002710d1
ramazan48 <77232741+ramazan48@users.noreply.github.com>
2024-07-11 17:27:04 +0300

    edited

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+var colorArray = 
+[ '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF',
+  '#FF0000', '#00FFFF', '#C0C0C0', '#00008B', '#7FFFD4',
+  '#ADD8E6',	'#FFA500', '#800080',	'#A52A2A','#FFC0CB',
+  '#FFFF00', '#800000', '#00FF00', '#008000', '#FF00FF',
+  '#808000'];
 var add = document.getElementById('addToDo');
 var input = document.getElementById('inputField');
 var toDoContainer = document.getElementById('toDoContainer');
-add.addEventListener('click',addItem);
+//add.addEventListener('click',addItem);
 input.addEventListener('keypress',function(e){
     if(e.key=="Enter" && input.value!= ""){
         addItem();
@@ -19,6 +34,10 @@ function addItem(e){
   else{
     alert("Task has been added");
   }
+  for(j=0; j<68;j++){
+    console.log(colorArray[j]);
+  }
+
   const item = document.createElement('div');
   item.classList.add('item');
 
@@ -27,26 +46,40 @@ function addItem(e){
   item.appendChild(item_content);
 
   const input_item = document.createElement('input');
-  input_item.classList.add('text','margarine-regular');
+  input_item.classList.add('text','margarine-regular','red');
   input_item.value = item_value;
   input_item.setAttribute('readonly', 'readonly');
   input_item.addEventListener('dblclick', function(){
     input_item.style.textDecoration = 'line-through';
+    input_item.style.textDecorationColor = "black";
     alert("The task has been completed");
   })
+
+  //Produce random numbers between 0 and max(input)
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  //It changes each input-text-background-color
+  input_item.style.color = "white";
+  input_item.style.backgroundColor = colorArray[getRandomInt(66)];
+  input_item.style.fontSize = "1.5vw"
 
   item_content.appendChild(input_item);
   const item_action = document.createElement('div');
   item_action.classList.add('actions');
 
-
   const edit_item = document.createElement('button');
   edit_item.classList.add('edit','btn','btn-success');
+  edit_item.style.fontSize = "1.5vw";
   edit_item.type = 'button';
   edit_item.innerText = 'Edit';
 
+
+
   const delete_item = document.createElement('buttonn');
+  delete_item.style.fontSize = "2vw";
   delete_item.classList.add('delete','btn','btn-danger','fa','fa-trash');
+  
 
   item_action.appendChild(edit_item);
   item_action.appendChild(delete_item);
@@ -55,6 +88,7 @@ function addItem(e){
 
   toDoContainer.appendChild(item);
 
+  //Editing tasks
   input.value = '';
   edit_item.addEventListener('click', (e) => {
     if(edit_item.innerText.toLowerCase() == "edit"){
@@ -63,6 +97,7 @@ function addItem(e){
       input_item.focus();
     }
     else{
+      //Edit input
       console.log(input_item.value);
       if(/^\s*$/.test(input_item.value)){
         alert("You can not add empty string");
@@ -74,6 +109,7 @@ function addItem(e){
     }  
   });
 
+  //Deleting tasks
   delete_item.addEventListener('click', (e) => {
     toDoContainer.removeChild(item);
     alert("Task has been deleted");
